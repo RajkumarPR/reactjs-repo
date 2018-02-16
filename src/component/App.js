2412abce4cd7f1eac2a71519daf57d32e52da281
WIN-KOJTB0PCIMH\rraigonde <raju.raigonde@gmail.com>
2018-02-16 12:34:00 +0530

    react stats

--- src/component/App.js
+++ src/component/App.js
@@ -6,13 +6,15 @@ import Header from './Header';
 import Footer from './Footer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {name : "Raj"}
+  }
   render() {
       return (
         <div className="App">
           <Header />
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          {this.state.name}
           <Footer />
         </div>
       );
--- src/component/Header.js
+++ src/component/Header.js
@@ -4,10 +4,12 @@ import logo from '../logo.svg';
 class Header extends Component {
 
   render() {
+    console.log(this.props)
     return (
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
+        <h1>{this.props.Title}</h1>
       </header>
     );
   }
